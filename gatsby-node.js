b2cbd267ef3e03b311cd9415909a1c62c4a6d5d2
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-08-25 14:05:42 +0200

    fix(loggin): remove console.log

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const SizePlugin = require('size-plugin')
 
 exports.onCreateWebpackConfig = ({ stage, actions }, options) => {
-  console.log({ stage, env: process.env.NODE_ENV })
   if (
     (process.env.NODE_ENV === 'production' && stage === 'build-javascript') ||
     options.development
